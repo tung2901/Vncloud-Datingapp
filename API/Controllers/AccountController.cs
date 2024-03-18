b565bd399dd9bfde074d9e419923c72242d37963
tunggg <tranhuutungkeu@gmail.com>
2024-03-18 12:04:06 +0700

    update sec4-b5

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        public AccountController() 
+        {
+            
+        }
+    }
+}
---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,17 @@
+// using API.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    // [ServiceFilter(typeof(LogUserActivity))]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController: ControllerBase
+    {
+
+    }
+}
