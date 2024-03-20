926aaf9f006bb77dd946eb7fe8b2f9f5fef61c86
tunggg <tranhuutungkeu@gmail.com>
2024-03-20 09:45:49 +0700

    update

--- API/Service/TokenService.cs
+++ API/Service/TokenService.cs
@@ -1,13 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+// using System.IdentityModel.Tokens.Jwt;
 
 namespace API.Service
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config) 
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokrnKey"]));
+        }
         public string createToken(AppUser user)
         {
-            throw new System.NotImplementedException();
+             var claims = new List<Claim>
+            {
+               new Claim(JwtRegisteredClaimNames),
+
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+            // var tokenHandler = new JwtSecurityTokenHandler();
+
+            // var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // return tokenHandler.WriteToken(token);
         }
     }
 }
