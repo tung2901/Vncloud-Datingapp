e3046f02dcc75ddc03110972517fa4ab79266e0c
tunggg <tranhuutungkeu@gmail.com>
2024-03-20 09:13:17 +0700

    update

--- API/Service/TokenService.cs
+++ API/Service/TokenService.cs
@@ -1,7 +1,13 @@
+using API.Entities;
+using API.Interfaces;
+
 namespace API.Service
 {
     public class TokenService : ITokenService
     {
-
+        public string createToken(AppUser user)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
