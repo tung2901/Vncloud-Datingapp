a86987a02f1189f079042b2770685a3df9ca6e43
tunggg <tranhuutungkeu@gmail.com>
2024-04-01 00:00:07 +0700

    update file buggy

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,53 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+
+            if (thing == null) return NotFound();
+
+            return Ok(thing);
+        }
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            try
+            {
+                var thing = _context.Users.Find(-1);
+
+                var thingToReturn = thing.ToString();
+
+                return thingToReturn;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Computer says no!");
+            }
+
+
+        }
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+    }
+}
