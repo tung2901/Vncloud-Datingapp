25150e98954404580c7e12aa1f2a4335610942f6
tunggg <tranhuutungkeu@gmail.com>
2024-04-08 18:57:09 +0700

    update section11 video 7

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -41,7 +41,7 @@ namespace API.Controllers
       }
 
 
-      [HttpGet("{username}")]
+      [HttpGet("{username}", Name = "GetUser")]
       public async Task<ActionResult<MemberDto>> GetUser(string username)
       {
          return await _userRepository.GetMemberAsync(username);
@@ -84,7 +84,10 @@ namespace API.Controllers
          user.Photos.Add(photo);
 
          if (await _userRepository.SaveAllAsync())
-            return _mapper.Map<PhotoDto>(photo);
+         {
+               return CreatedAtRoute("GetUser", new{Username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+         }
+         
 
          return BadRequest("Problem adding photo");
       }
