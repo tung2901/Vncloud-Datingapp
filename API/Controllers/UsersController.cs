23b81cb2fe2cdcb3e559b370d07fe3381f35f2ec
tunggg <tranhuutungkeu@gmail.com>
2024-04-08 18:25:57 +0700

    update section11 video 6

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,57 +5,88 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]
-    public class UsersController : BaseApiController
-    {
-        private readonly IUserRepository _userRepository;
-        private readonly IMapper _mapper;
-        // private object usersToReturn;
+   [Authorize]
+   public class UsersController : BaseApiController
+   {
+      private readonly IUserRepository _userRepository;
+      private readonly IMapper _mapper;
+      // private object usersToReturn;
+      private readonly IPhotoService _photoService;
 
-        public UsersController(IUserRepository userRepository, IMapper mapper) 
-        {
-            _mapper = mapper;
-            _userRepository = userRepository;
-        }
+      public UsersController(IUserRepository userRepository, IMapper mapper,
+       IPhotoService photoService)
+      {
+         _photoService = photoService;
+         _mapper = mapper;
+         _userRepository = userRepository;
+      }
 
-        //api/user/3
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-        {
-           var users = await _userRepository.GetMembersAsync();
-           
-           return Ok(users) ;
-        }
-        
-      
-         [HttpGet("{username}")]
-        public async Task<ActionResult<MemberDto>> GetUser(string username)
-        {
-           return await _userRepository.GetMemberAsync(username);
+      //api/user/3
+      [HttpGet]
+      public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+      {
+         var users = await _userRepository.GetMembersAsync();
 
-        }
-         [HttpPut]
-         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+         return Ok(users);
+      }
+
+
+      [HttpGet("{username}")]
+      public async Task<ActionResult<MemberDto>> GetUser(string username)
+      {
+         return await _userRepository.GetMemberAsync(username);
+
+      }
+      [HttpPut]
+      public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+      {
+         var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+         _mapper.Map(memberUpdateDto, user);
+
+         _userRepository.Update(user);
+
+         if (await _userRepository.SaveAllAsync()) return NoContent();
+
+         return BadRequest("Failed to update user");
+      }
+
+      [HttpPost("add-photo")]
+      public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+      {
+         var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+         var result = await _photoService.AddPhotoAsync(file);
+
+         if (result.Error != null) return BadRequest(result.Error.Message);
+
+         var photo = new Photo
          {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var user = await _userRepository.GetUserByUsernameAsync(username);
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId
+         };
 
-            _mapper.Map(memberUpdateDto, user);
-
-            _userRepository.Update(user);
-
-            if (await _userRepository.SaveAllAsync()) return NoContent();
-
-            return BadRequest("Failed to update user");
+         if (user.Photos.Count == 0)
+         {
+            photo.IsMain = true;
          }
-    }
-    
+
+         user.Photos.Add(photo);
+
+         if (await _userRepository.SaveAllAsync())
+            return _mapper.Map<PhotoDto>(photo);
+
+         return BadRequest("Problem adding photo");
+      }
+   }
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Helpers;
 using API.Interfaces;
+using API.Service;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -12,13 +13,16 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
+
             return services;
         }
     }
