c6d654c23906fd91c940594ab55efa6f8d54bcf8
tunggg <tranhuutungkeu@gmail.com>
2024-04-30 12:29:03 +0700

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -22,9 +22,7 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CaculateAge();
-        // }
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
